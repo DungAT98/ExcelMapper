eeac645f904f04a36178dabe35a2b95063ddbd85
DungAT <dungat98@gmail.com>
2022-09-12 09:52:42 +0700

    Add method get header row in excel

--- ExcelReaderMapper/Model/MappingSetting.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ExcelReaderMapper.Model
-{
-    public class MappingSetting
-    {
-        public int LineOffset { get; set; } = 1;
-
-        public int NumberOfHeaderRow { get; set; } = 1;
-    }
-}
---
+++ ExcelReaderMapper/Model/WorksheetHeaderInformation.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ExcelReaderMapper.Model
+{
+    public class WorksheetHeaderInformation : WorksheetBaseInformation
+    {
+        public List<string> HeaderRows { get; set; }
+    }
+}
